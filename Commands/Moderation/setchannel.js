a0ef6b4c5d3941f8fc6de0b43e153be51fe83018
SCrbnll <samuelcarbonell1203@gmail.com>
2024-07-31 13:35:12 +0200

    [NEW] slowmode command

--- Commands/Moderation/setchannel.js
+++ Commands/Moderation/setchannel.js
@@ -1,21 +1,31 @@
-const { SlashCommandBuilder, PermissionsBitField  } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const config = require('./../../Utils/config.js');
 const updateConfig = require('./../../Utils/updateConfig.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('setchannel')
-        .setDescription('Establece un canal específico.')
-        .addStringOption(option =>
+    data: (() => {
+        const command = new SlashCommandBuilder()
+            .setName('setchannel')
+            .setDescription('Establece un canal específico.')
+            .addChannelOption(option =>
+                option.setName('canal')
+                    .setDescription('El canal que se utilizará.')
+                    .setRequired(true)
+            );
+
+        const channelOptions = Object.keys(config.channels).map(key => {
+            return { name: key, value: key };
+        });
+
+        command.addStringOption(option =>
             option.setName('tipo')
-                .setDescription('El tipo de canal (e.g., welcome, goodbye).')
+                .setDescription('El tipo de canal')
                 .setRequired(true)
-        )
-        .addChannelOption(option =>
-            option.setName('canal')
-                .setDescription('El canal que se utilizará.')
-                .setRequired(true)
-        ),
+                .addChoices(...channelOptions)
+        );
+
+        return command;
+    })(),
     async execute(interaction) {
         const channelType = interaction.options.getString('tipo');
         const channel = interaction.options.getChannel('canal');
@@ -23,15 +33,10 @@ module.exports = {
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
             return interaction.reply({ content: 'No tienes permisos para usar este comando.', ephemeral: true });
         }
-        if (!config.channels.hasOwnProperty(channelType)) {
-            return interaction.reply({ content: 'Tipo de canal no válido, los canales estabecidos son\n> `\ welcome \` `\ goodbye \` `\ logs \` `\ boost \` `\ birthday \` `\ alliance \` `\ support \` `\ suggestion \`', ephemeral: true });
-        }
 
-        console.log(`ID antes del cambio (${channelType}): `, config.channels[channelType]);
         config.channels[channelType] = channel.id;
         updateConfig(channelType, channel.id);
 
         await interaction.reply(`El canal para ${channelType} ha sido establecido a ${channel}.`);
-        console.log(`ID después del cambio (${channelType}): `, config.channels[channelType]);
     },
 };
---
+++ Commands/Moderation/slowmode.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionsBitField  } = require('discord.js');
+const config = require('./../../Utils/config.js');
+const updateConfig = require('./../../Utils/updateConfig.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('slowmode')
+        .setDescription('Establece un slowmode en un canal específico.')
+        .addChannelOption(option =>
+            option.setName('canal')
+                .setDescription('El canal que se editará.')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
+            option.setName('segundos')
+                .setDescription('Segundos a establecer.')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const channel = interaction.options.getChannel('canal');
+        const seg = interaction.options.getInteger('segundos')
+
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return interaction.reply({ content: 'No tienes permisos para usar este comando.', ephemeral: true });
+        }
+        
+        channel.setRateLimitPerUser(seg).then(async () => {
+            await interaction.reply(`Se ha establecido un slowmode de **${seg}** segundos en el canal ${channel}`);
+        });      
+    },
+};
