d7cf91e9b2e8c4c02faafdb5a5e61f10bb52a528
SCrbnll <samuelcarbonell1203@gmail.com>
2024-07-31 13:39:48 +0200

    [MOD] ping command

--- Commands/Info/ping.js
+++ Commands/Info/ping.js
@@ -1,11 +1,13 @@
-// commands/ping.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Muestra latencia del bot'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+        async execute(interaction) {
+            const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+            const latency = sent.createdTimestamp - interaction.createdTimestamp;
+            const apiLatency = Math.round(interaction.client.ws.ping);
+            await interaction.editReply(`🏓 Latencia es ${latency}ms. Latencia de API es ${apiLatency}ms`);
+        },
 };
